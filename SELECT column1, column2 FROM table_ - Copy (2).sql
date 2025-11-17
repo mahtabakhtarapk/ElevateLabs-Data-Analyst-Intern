e74b04bc1c11efb961a7272005affba4b731ade2
mahtabakhtarapk <ayaanzeya123@gmail.com>
2025-11-17 20:35:51 +0530

    Add files via upload

---
+++ SELECT column1, column2 FROM table_ - Copy (2).sql
@@ -0,0 +1,7 @@
+﻿    SELECT column1, column2 FROM table_name;
+    SELECT * FROM table_name;
+    SELECT * FROM employees WHERE department = 'Sales' AND salary > 50000;
+    SELECT product_name, price FROM products ORDER BY price DESC; 
+    SELECT department, AVG(salary) FROM employees GROUP BY department;
+    SELECT department, COUNT(employee_id) FROM employees GROUP BY department HAVING            COUNT(employee_id) > 10;
+    SELECT o.order_id, c.customer_name FROM orders o JOIN customers c ON o.customer_id = c.customer_id;
